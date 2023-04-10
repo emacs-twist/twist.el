c844df52299c8e15034979ffd08cbab923370185
Akira Komamura <akira.komamura@gmail.com>
2023-04-11 03:13:29 +0900

    Add lint

---
+++ .nomake/flake.nix
@@ -0,0 +1,13 @@
+{
+  description =
+    "THIS IS AN AUTO-GENERATED FILE. PLEASE DON'T EDIT IT MANUALLY.";
+  inputs = {
+    twist = {
+      flake = false;
+      owner = "emacs-twist";
+      repo = "twist.el";
+      type = "github";
+    };
+  };
+  outputs = { ... }: { };
+}
